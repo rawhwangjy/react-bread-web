48b1872e977796dd92a9c2fa0e62f0294807cdbf
breadgee <rawhwangjy@gmail.com>
2023-07-19 13:54:52 +0900

    board store 추가

---
+++ src/reducers/board-slice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const boardSlice = createSlice({
+	name: 'board',
+	initialState: {
+		isLoading: false,
+		isError: false,
+		boardList: [],
+		boardView: {},
+	},
+	reducers: {
+		isLoading(state, action) {
+			state.isLoading = action.payload.isLoading;
+			console.log();
+		},
+		isError(state, action) {
+			state.isError = action.payload.isError;
+		},
+		getBoardList(state, action) {
+			state.boardList = [...action.payload.boardList];
+		},
+		getBoard(state, action) {
+			state.boardView = action.payload.boardView[0];
+		},
+	},
+});
+
+export const boardActions = boardSlice.actions;
+
+export default boardSlice;
