ea57afabb6ce64d8214099f9bff98b646a7a318b
breadgee <rawhwangjy@gmail.com>
2023-07-19 13:54:14 +0900

    공통 상수 추가

---
+++ src/utils/common.constants.js
@@ -0,0 +1,18 @@
+export const RequestType = {
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT',
+	UPDATE: 'UPDATE',
+	DELETE: 'DELETE',
+};
+
+export const API_URL = process.env.REACT_APP_API_URL;
+
+export const LocalKey = {
+	token: 'TOKEN',
+	lastPageNum: 'lastPageNum',
+	lastProjectOffsetY: 'lastProjectOffsetY',
+	goProjectList: 'goProjectList',
+	accessToken: 'accessToken',
+	refreshToken: 'refreshToken',
+};
