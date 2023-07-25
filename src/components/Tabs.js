e1611c3b428dd175b609a7ba25306acd84bcfafc
breadgee <rawhwangjy@gmail.com>
2023-07-25 12:40:35 +0900

    Tab visibility 수정

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,14 +1,55 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useReducer, useEffect } from 'react';
+
+const reducerFn = (state, action) => {
+	switch (action.type) {
+		case 'SET_SWIPER_WIDTH':
+			return { ...state, tabWidth: action.payload };
+		case 'CURRENT_SLIDE':
+			const newState = {
+				...state,
+				selectedTab: action.payload,
+			};
+			return newState;
+		case 'CLICK':
+			const clickState = {
+				...state,
+				currentPosition: -(state.selectedTab * state.tabWidth),
+			};
+			return clickState;
+		default:
+			return state;
+	}
+};
 
 const Tabs = ({ tabTitles, children }) => {
-	const [selectedTab, setSelectedTab] = useState(0);
-
 	const [maxHeight, setMaxHeight] = useState(null);
+	const tabRef = useRef(null);
 	const tabRefs = useRef([]);
 
-	const selectTab = (index) => {
-		setSelectedTab(index);
+	const initialState = {
+		selectedTab: 0,
+		tabWidth: 0,
+		currentPosition: 0,
 	};
+
+	const [state, dispatchFn] = useReducer(reducerFn, initialState);
+
+	const selectTab = (index) => {
+		dispatchFn({
+			type: 'CURRENT_SLIDE',
+			payload: index,
+		});
+		dispatchFn({
+			type: 'CLICK',
+		});
+	};
+
+	useEffect(() => {
+		dispatchFn({
+			type: 'SET_SWIPER_WIDTH',
+			payload: tabRef.current.clientWidth,
+		});
+	}, []);
 
 	const loadData = () => {
 		const tabHeights = tabRefs.current.map((ref) => ref.clientHeight);
@@ -16,15 +57,16 @@ const Tabs = ({ tabTitles, children }) => {
 		setMaxHeight(maxHeight);
 	};
 
-	console.log('childrenchildrenchildrenchildren', children.length);
-
 	return (
-		<div className='tab_wrap'>
+		<div
+			className='tab_wrap'
+			ref={tabRef}
+		>
 			<div className='tabs'>
 				{tabTitles.map((title, index) => {
 					return (
 						<div
-							className={`tab ${selectedTab === index ? 'active' : ''}`}
+							className={`tab ${state.selectedTab === index ? 'active' : ''}`}
 							key={`tab${index}`}
 						>
 							<button
@@ -39,14 +81,20 @@ const Tabs = ({ tabTitles, children }) => {
 			</div>
 			<div
 				className='tab_panels'
-				style={{ height: maxHeight + 'px' }}
+				// style={{ height: maxHeight + 'px' }}
+				style={{
+					height: maxHeight + 'px',
+					transform: `translate3d(${state.currentPosition}px, 0, 0)`,
+				}}
 			>
 				{children.length > 1 &&
 					children.map((tab, index) => (
 						<div
 							key={`tabPanel${index}`}
 							ref={(ref) => (tabRefs.current[index] = ref)}
-							className={`tab_panel ${selectedTab === index ? 'active' : ''}`}
+							className={`tab_panel ${
+								state.selectedTab === index ? 'active' : ''
+							}`}
 							onLoad={loadData}
 						>
 							{tab}
