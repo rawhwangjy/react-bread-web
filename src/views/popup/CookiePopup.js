47c02958564f388477db61fd7337a0a90fdd4c70
breadgee <rawhwangjy@gmail.com>
2023-07-25 15:47:38 +0900

    소스 정리

--- src/views/popup/CookiePopup.js
+++ src/views/popup/CookiePopup.js
@@ -18,39 +18,37 @@ const CookiePopup = ({ onChange }) => {
 	};
 
 	return (
-		<>
-			<div className='cookie_popup'>
-				<div className='cookie_content'>
-					<div className='content'>
-						<p>이 사이트는 React 프레임워크를 이용해 만들었습니다.</p>
-						<a
-							href='http://breadg.dothome.co.kr/'
-							target='_blank'
-							role='button'
-							rel='noreferrer'
-							className='btn sm primary border'
-						>
-							Vue3 포트폴리오 사이트 바로가기
-						</a>
-					</div>
-					<div className='cookie_btns'>
-						<Checkbox
-							label='1시간동안 보지 않기'
-							styles='circle'
-							onChange={changeCheckbox}
-						/>
-						<button
-							type='button'
-							className='btn sm light'
-							onClick={closePopup}
-						>
-							<span>확인</span>
-						</button>
-					</div>
+		<div className='cookie_popup'>
+			<div className='cookie_content'>
+				<div className='content'>
+					<p>이 사이트는 React 프레임워크를 이용해 만들었습니다.</p>
+					<a
+						href='http://breadg.dothome.co.kr/'
+						target='_blank'
+						role='button'
+						rel='noreferrer'
+						className='btn sm primary border'
+					>
+						Vue3 포트폴리오 사이트 바로가기
+					</a>
 				</div>
-				<span className='dim' />
+				<div className='cookie_btns'>
+					<Checkbox
+						label='1시간동안 보지 않기'
+						styles='circle'
+						onChange={changeCheckbox}
+					/>
+					<button
+						type='button'
+						className='btn sm light'
+						onClick={closePopup}
+					>
+						<span>확인</span>
+					</button>
+				</div>
 			</div>
-		</>
+			<span className='dim' />
+		</div>
 	);
 };
 
